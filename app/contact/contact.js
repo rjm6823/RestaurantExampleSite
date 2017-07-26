9c182c35916d68bc4c92a311773f24141ff67b42
RJ Madsen <rjm6823@rit.edu>
2017-07-26 11:46:59 -0400

    Google maps integration

--- app/contact/contact.js
+++ app/contact/contact.js
@@ -10,5 +10,15 @@ angular.module('myApp.contact', ['ngRoute'])
     }])
 
     .controller('ContactCtrl', [function () {
-
+        $(document).ready(function () {
+            var place = {lat: 35.9940, lng: -78.8986};
+            var map = new google.maps.Map(document.getElementById('map'), {
+                zoom: 11,
+                center: place
+            });
+            var marker = new google.maps.Marker({
+                position: place,
+                map: map
+            });
+        });
     }]);
